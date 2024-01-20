6b1241899342df3b9de3c658867fb6a2dff32b59
Beril (Beth) <90333992+bberildundar@users.noreply.github.com>
2024-01-20 21:42:58 +0100

    make edit users button for admins

--- app/views/header.php
+++ app/views/header.php
@@ -2,25 +2,28 @@
 <html lang="en">
 
 <head>
-    <title>SHAREEE!</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+  <title>SHAREEE!</title>
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="bg-gray-200">
-    <main>
-        <nav class="bg-black p-6">
-            <div class="container mx-auto flex justify-between items-center">
-                <div class="flex items-center">
-                    <a href="/" class="text-white text-4xl font-semibold mr-4">SHAREEE!</a>
-                    <a href="/" class="text-white hover:text-gray-300 ml-9 font-bold text-lg">Home</a>
-                    <?php
+  <main>
+    <nav class="bg-black p-6">
+      <div class="container mx-auto flex justify-between items-center">
+        <div class="flex items-center">
+          <a href="/" class="text-white text-4xl font-semibold mr-4">SHAREEE!</a>
+          <a href="/" class="text-white hover:text-gray-300 ml-9 font-bold text-lg">Home</a>
+          <?php
           if (isset($_SESSION['user_id']) && $_SESSION['user_id'] !== null) {
             echo '<a href="/profile" class="text-white hover:text-gray-300 ml-9 font-bold text-lg">Profile</a>';
           }
+          if (isset($_SESSION['user_id']) && $_SESSION['user_id'] !== null && $_SESSION['user_role'] === true) {
+            echo '<a href="/admin/users" class="text-white hover:text-gray-300 ml-9 font-bold text-lg">Edit Users</a>';
+          }
           ?>
-                </div>
-                <div class="space-x-4">
-                    <?php
+        </div>
+        <div class="space-x-4">
+          <?php
           if (!isset($_SESSION['user_id']) || $_SESSION['user_id'] === null) {
             echo '<a href="/auth/register" class="text-white hover:bg-rose-300 bg-rose-400 px-4 py-2">Create Account</a>';
             echo '<a href="/auth/login" class="text-white hover:bg-rose-800 bg-rose-900 px-4 py-2">Login</a>';
@@ -29,6 +32,6 @@
             echo '<a href="/auth/logout" class="text-white hover:bg-red-600 bg-red-500 px-4 py-2">Logout</a>';
           }
           ?>
-                </div>
-            </div>
-        </nav>
+        </div>
+      </div>
+    </nav>
